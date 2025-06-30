7540959e350625b43fb84614533e42b68b7943a4
finkash <eva.knezevik@gmail.com>
2025-06-30 17:07:48 +0200

    Update Task.java

--- BackEnd/src/main/java/org/example/Task.java
+++ BackEnd/src/main/java/org/example/Task.java
@@ -2,17 +2,38 @@ package org.example;
 
 public class Task {
     private String ToDo;
+    private LocalDate completedAt;
+    private List<String> classes;
 
     public Task (String ToDo) {
         this.ToDo = ToDo;
     }
-    public Task (){
+    public Task (String ToDo, LocalDate completedAt, List<String> classes){
+        this.ToDo=ToDo;
+        this.completedAt = completedAt;
+        this.classes = classes;
     }
+    public Task() {}
 
     public String getToDo() {
         return ToDo;
     }
     public void setToDo(String toDo) {
         ToDo = toDo;
+    }
+    public LocalDate getCompletedAt() {
+        return completedAt;
+    }
+
+    public void setCompletedAt(LocalDate completedAt) {
+        this.completedAt = completedAt;
+    }
+
+    public List<String> getClasses() {
+        return classes;
+    }
+
+    public void setClasses(List<String> classes) {
+        this.classes = classes;
     }
 }
