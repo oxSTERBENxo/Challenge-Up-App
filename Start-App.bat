3d6937a1830097ba03d98304a3fadc3936c45d0a
duckpop <blagoja.stojev@students.finki.ukim.mk>
2025-07-05 02:53:38 +0200

    edited bash file a bit

--- Start-App.bat
+++ Start-App.bat
@@ -16,9 +16,11 @@ if errorlevel 1 (
 
 REM Run Java server with classpath including gson and compiled classes folder
 start /B java -cp "%SRC%\classes;%LIBS%" org.example.Server
+start "" "http://localhost:5005"
 
 REM Run Blazor app
 dotnet run --project ChallengeUpFrontend\ChallengeUpFrontend.csproj
+
 
 REM Kill the server process as before
 for /f "tokens=2 delims=," %%a in ('wmic process where "CommandLine like '%%org.example.Server%%'" get ProcessId /format:csv ^| findstr /i "java"') do taskkill /PID %%a /F
